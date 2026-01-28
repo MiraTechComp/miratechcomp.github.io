ac2b2f7190bb25523135b1bf7685c8265b691693
NThacker5246 <nytrecover@gmail.com>
2026-01-28 09:03:03 +0400

    Testing Cookies

--- account-main.js
+++ account-main.js
@@ -1,5 +1,6 @@
 var userhello = document.getElementById('userhello');
 var profile_image = document.getElementById('profile_image');
+var logoff = document.getElementById('logoff');
 // let user_name = document.URL.substring(document.URL.indexOf('?')+1);
 // if(user_name != document.URL){
 
@@ -12,3 +13,8 @@ if(document.cookie != ""){
 		profile_image.src = v1.avatarUrl;
 	});
 }
+
+logoff.addEventListener("click", function(e) {
+	document.cookie = "token=token; max-age=-1";
+	location.href = "./login.html";
+});
